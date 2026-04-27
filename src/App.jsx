5e1dca299f4dee08cf8c79d8f68fed2ddfbaec38
joaquin vulcano <vulcanojoaquin@gmail.com>
2026-04-27 18:02:59 -0300

    footer integrado en app

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './Components/Header/Header';
 import Home from './Pages/Home/Home';
+import Detalles from './Pages/Details/Details';
+import Favoritos from './Pages/Favorites/Favorites';
+import Footer from './Footer/Footer';
+import Error404 from './Pages/Error404/Error404';
+
 // import AppRoutes from './routes/AppRoutes'; // Descartado para mantener el estado de búsqueda global
 
 function App() {
@@ -21,7 +26,10 @@ function App() {
         <Route path="/" element={<Home searchQuery={searchQuery} />} />
         <Route path="/favoritos" element={<Favoritos />} />
         <Route path="/detalles/:id" element={<Detalles />} />
+        <Route path="*" element={<Error404 />} />
       </Routes>
+
+      <Footer />
     </Router>
   );
 }
