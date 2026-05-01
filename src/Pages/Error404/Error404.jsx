0fc7c6411dfdf119d7fd77dd0e64d89afc60f00d
Fidel Pizarro <fidelpizarro11@gmail.com>
2026-05-01 18:35:13 -0300

    incorpore cambios para que cuando se busque en detalles un id que no existe muestre el componente Error404

--- src/Pages/Error404/Error404.jsx
+++ src/Pages/Error404/Error404.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Error404 = () => {
     return (
-        <div className="flex flex-col items-center justify-center h-full text-center p-10 mt-20">
+        <div className="flex flex-col items-center justify-center h-full text-center p-10 mt-20" >
             <h1 className="text-8xl font-bold text-red-500 mb-4">404</h1>
             <h2 className="text-3xl font-semibold text-gray-800 mb-6">Oops! Página no encontrada</h2>
             <p className="text-gray-500 mb-8 max-w-md">
--- src/services/apodService.js
+++ src/services/apodService.js
@@ -1,9 +1,11 @@
+import { data } from "react-router-dom";
+
 const BASE_URL = 'https://69eb6fd597482ad5c527b5ee.mockapi.io/api/planetas/APOD'
 
 
-// Le agregamos el parámetro searchQuery por defecto vacío
+// le agregamos el parámetro searchQuery por defecto vacío
 export const getAllApod = async (page = 1, limit = 10, searchQuery = '') => { // si no se pasa valor por parametro, se usa por default 1 al 10
-    // Siempre enviamos paginación; si hay búsqueda, filtramos solo por título
+    // siempre enviamos paginación; si hay búsqueda, filtramos solo por título
     let url = `${BASE_URL}?page=${page}&limit=${limit}`;
     if (searchQuery) {
         url += `&title=${searchQuery}`;
@@ -12,8 +14,8 @@ export const getAllApod = async (page = 1, limit = 10, searchQuery = '') => { //
     const response = await fetch(url);
     
     // SOLUCIÓN AL BUG: Si MockAPI no encuentra nada, devuelve un error 404.
-    // Si no atajamos esto, intenta hacer un map sobre un string "Not found" y se rompe.
-    // Si no hay respuesta OK, devolvemos un array vacío para que React no falle.
+    // si no atajamos esto, intenta hacer un map sobre un string "Not found" y se rompe.
+    // si no hay respuesta OK, devolvemos un array vacío para que React no falle.
     if (!response.ok) {
         return [];
     }
@@ -23,5 +25,8 @@ export const getAllApod = async (page = 1, limit = 10, searchQuery = '') => { //
 
 export const getApodById = async (id) => {
     const response = await fetch(`${BASE_URL}/${id}`);
+    if (!response.ok){
+        return null
+    }
     return response.json();
 }
